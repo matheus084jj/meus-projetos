62b9d88ee1c3a097185e75b3168424f5c143bb9a
matheus084jj <matheus084jj@gmail.com>
2026-06-16 13:57:47 -0300

    Add files via upload

---
+++ estd funções/funcoes.js
@@ -0,0 +1,38 @@
+
+
+/* let valor = 20
+
+function incrementa() {
+    valor = 20 + 30
+}
+
+incrementa()
+console.log(valor) */
+
+
+
+
+let valore = 20
+
+function soma(numero1, numero2) {
+    const SomaDosNumeros = numero1 + numero2
+
+    return SomaDosNumeros
+}
+
+const meusNumeros =soma(5, 2)
+
+
+console.log(meusNumeros)
+
+
+
+
+const  multi = (numero1, numero2) => {
+    const multplicação = numero1 * numero2
+    return multplicação
+}
+
+const multiplicação = multi(5, 2)
+
+console.log(multiplicação)
---
+++ estd funções/treino.js
@@ -0,0 +1,22 @@
+function divisão(numero1, numero2, numero3) {
+    const divisão= numero1 / numero2 / numero3
+    return divisão
+}
+
+const divisao = divisão(10, 5, 5)
+
+console.log(divisao)
+
+
+const subtração = (number1, number2) => number1 - number2
+
+const sub = subtração(5, 8)
+console.log(sub)
+
+
+const raizqudr = (numero1, numero2) => numero1 / numero2 
+
+const rsqr = raizqudr(42, 3)
+
+console.log(rsqr)
+
